4fdb1556dc81b378b72c5b34c3ab4f720bff857b
Ashiq <arahmangt10@gmail.com>
2018-01-29 19:35:47 -0500

    WIP

---
+++ server.js
@@ -0,0 +1,20 @@
+// Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
